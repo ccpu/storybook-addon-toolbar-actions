ab0bdd8f5f8668ad19f097817825ba30df6b017e
ccpu <m.doaie@hotmail.com>
2020-06-22 15:46:39 +0430

    added babel-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,19 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('ts-loader'),
+    test: /\.tsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
       },
-    ],
+    },
+    exclude: [/node_modules/],
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
